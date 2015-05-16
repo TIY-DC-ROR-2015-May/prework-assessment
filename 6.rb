67868d2027a485c56790bea8916ccc2f27691c61
Polly Sileo <pollysileo@gmail.com>
2015-05-16 16:41:23 -0400

    add person and favorites

--- 6.rb
+++ 6.rb
@@ -1,4 +1,5 @@
 class Person
+  attr_reader :favorite_things
   def initialize first_name, last_name
     @first_name      = first_name
     @last_name       = last_name
@@ -12,10 +13,16 @@ class Person
   # Implement the following functions
 
   def name
-    puts "#{@first_name}, #{@last_name}"
+    "#{@first_name}, #{@last_name}"
   end
 
   def shared_interests_with other_person
+    @share = []
+    @favorite_things.each do |fav|
+      if other_person.favorite_things.include?(fav) then
+        @share << fav
+      end
+    end
   end
 end
 
@@ -38,6 +45,16 @@ last_name = gets.chomp
 you = Person.new first_name, last_name
 puts "Cool. Welcome, #{you.name}."
 puts "What are some of your favorite things? (separated with ,-s)"
+favs = gets.chomp
+favs.split.each do |fav|
+  you.add_favorite fav
+end
+
+
 
 # TODO: finish this ...
 # find our shared interests, if any, and print out something about them
+
+puts "Shared likes #{you.shared_interests_with james.to_s }"
+
+
